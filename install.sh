853524db663261c308d2d7a65d203e8893912075
yokoshima <skurek@forus.ru>
2026-04-02 10:12:10 +0800

    fix: updated install.sh, now correct installation

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-REPO="https://github.com/yokoshima/myapp.git"
-BINARY="myapp"
+REPO="https://github.com/sakashimaa/rsmon.git"
+BINARY="rsmon"
 
 # check rust
 if ! command -v cargo >/dev/null 2>&1; then
